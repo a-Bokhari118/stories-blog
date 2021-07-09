4396f5f965a6749a1005358377a0384e676ce8cd
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-07-09 12:30:41 +0300

    register and login

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -13,6 +15,8 @@ mongoose
   })
   .then(console.log('DB Conntected'))
   .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute);
 
 app.listen('5000', () => {
   console.log('Server is running');
